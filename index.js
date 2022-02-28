609f5d9b25718b3942c6486a7b7fb26b846da5bb
LeeSuengSub <zlzm2522@gmail.com>
2022-02-28 15:22:58 +0900

    feat: Java_Script 처음 배운날

---
+++ index.js
@@ -0,0 +1,28 @@
+// JAVA print
+// FileSystem.out.println("Hello World");
+// 자바스크립트 print
+console.log("Hello World")
+// 세미콜론 권장사항이지 필수사항은 아니다.
+var x=10;
+var y='안녕하세요';
+console.log('x의 값은? '+x)
+console.log('y의 값은? '+y)
+
+var num = 10;
+var num2 = 20;
+var result = num+num2
+console.log(result)
+if(result==30){
+    console.log('30 입니다.')
+}
+for(var i=0; i<10; i++){
+    console.log(i)
+}
+for(var i=0; i<=10; i++){
+    if((i%2)==0){
+        console.log(i+'은 짝수 입니다.')
+    }
+}
+for(var i=0; i<6; i++){
+    console.log('*'.repeat(i+1))
+}
